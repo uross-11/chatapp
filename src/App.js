4d79a1da9f091993df69ec1a4e004f7dec348ff2
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2022-04-11 22:01:00 +0200

    Add Firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import './App.css';
 
+// Firebase SDK
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Firebase Hooks
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB1Q_zUfp3EFZbJnf7x-srEi7wBB4Et288",
+  authDomain: "superchat-fe583.firebaseapp.com",
+  projectId: "superchat-fe583",
+  storageBucket: "superchat-fe583.appspot.com",
+  messagingSenderId: "181567628749",
+  appId: "1:181567628749:web:34278ff52c6b8ba75e1084"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
   return (
     <div className="App">
